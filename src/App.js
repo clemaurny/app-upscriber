ddaf16b3d4aa10a26051b6f05fc76206d550161e
clement <clemansles@gmail.com>
2018-03-05 22:53:57 +0100

    ocr cloud service and parsing text content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 import Medium  from "./components/UpScriber/medium";
 import ToolBar from "./components/UpScriber/toolbar";
 import Upload from "./components/UpScriber/upload";
 import Home from "./components/UpScriber/home"
+
 import {
   BrowserRouter as Router,
   Route,
